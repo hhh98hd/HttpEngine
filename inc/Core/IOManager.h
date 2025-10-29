468f6a7a5baa99190ef2cc001f9ab542058f674f
Huy Hoang Hoang <hhhoang498@gmail.com>
2025-10-29 13:48:06 +1100

    Add ConnectionState struct

--- inc/Core/IOManager.h
+++ inc/Core/IOManager.h
@@ -1,19 +1,37 @@
 #ifndef IO_MANAGER_H
 #define IO_MANAGER_H
 
+#include <string>
+#include <unordered_map>
+
 class IOManager {
 public:
     static IOManager* getInstance();
     void run(int port);
 
 private:
+    struct ConnectionState
+    {
+        std::string requestHeader;
+        bool headerCompleted;
+        bool hasBody;
+        bool bodyCompleted;
+        size_t totalBodyBytesReceived;
+
+        ConnectionState()
+            : headerCompleted(false), hasBody(false), bodyCompleted(false), totalBodyBytesReceived(0) {}
+    };
+
     IOManager();
     ~IOManager();
     void init(int port);
 
     static IOManager* m_instance;
+
     int m_serverFd;
     int m_epollFd;
+
+    std::unordered_map<int, ConnectionState> m_connections;
 };
 
 #endif
